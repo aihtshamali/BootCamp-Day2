fc75596ea4d40ce81b90138ea94e7c9a65567048
Aihtsham Ali <aihtshamali@gmail.com>
2020-06-19 11:51:57 +0500

    ReactStatesDone

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './css/index.css';
-// import Skill from './Skill'
-import MediaCard from './Components/MediaCard/MediaCard';
-import Gate from './Components/Gate/Gate';
 import Room from './Components/Room/Room';
 
 
