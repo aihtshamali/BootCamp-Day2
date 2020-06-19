4b6e104603ef1be4830126d907f3567da40a858c
Aihtsham Ali <aihtshamali@gmail.com>
2020-06-19 11:48:29 +0500

    ReactStatesDone

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './css/index.css';
 // import Skill from './Skill'
 import MediaCard from './Components/MediaCard/MediaCard';
 import Gate from './Components/Gate/Gate';
+import Room from './Components/Room/Room';
+
+
 
 function App({name}) {
   return (
     <div className="App">
       <header className="App-header">
-         <p class="tp-">
+         <p >
            Hello My name is <strong>{name}</strong>
          </p>
-        <MediaCard title="Title of MediaCard" body={<span>Here is the simple body with<br/><strong>Bolder Content</strong></span>} imageUrl="https://media-exp1.licdn.com/dms/image/C5603AQHUro7XOJrLCw/profile-displayphoto-shrink_200_200/0?e=1597881600&v=beta&t=qk7hG-xoL03XE7qxXiAkSWMJBRsOWhhoIQ3yiuHhdz4" />         
-        <Gate isOpen={false}/>
-        <Gate isOpen={true}/>
+        <Room/>
        </header>
     </div>
   );
---
+++ src/Components/Room/Room.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+import './Room.css'
+
+function Room({}){
+    const [isLit, setLit] = useState(true);
+    let [RoomTemp, setTemp] = useState(72);
+    return (
+        <div className={`room ${isLit ? "lit" : "dark"}`}>
+                <button onClick={() => setLit(true)}>Light</button> - 
+                <button onClick={() => setLit(false)}>Dark</button>
+                <p>
+                    Room Temperature: {RoomTemp} F
+                </p>
+                <span>Set the Temp: <button onClick={() => setTemp(++RoomTemp)}>+</button>  <button onClick={() => setTemp(--RoomTemp)}>-</button></span>
+                {/* <span>Decrease the Temp: <button onClick={() => setTemp(--RoomTemp)}>-</button></span> */}
+        </div>
+    )
+}
+export default Room
